a98cd5ebbdf1e8960be76cab21f115b2c834ff02
Norbert305 <sealsnorbert@yahoo.com>
2022-02-17 04:42:51 +0000

    game start

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,19 +6,20 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="game1">
+				<section>
+					<article>
+						<p>
+							Want to receive the latest news on games!!! Please
+							sign up now!!! You will enjoy the awsome content
+							provided to you!! The best games from the past,
+							present and future are here
+							nooooowwwwwwwwwwwww!!!!!!!!!!
+						</p>
+					</article>
+				</section>
+			</div>
 		</div>
 	);
 };
