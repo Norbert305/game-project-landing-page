a35e35e4a1205afdb1ce9f83069bb768be5c361b
Norbert305 <sealsnorbert@yahoo.com>
2022-02-17 22:58:02 +0000

    changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -50,18 +50,20 @@ const Home = () => {
 			<div classNameName="game1" id="home">
 				<section>
 					<article>
+						<h1>Sign Up Now</h1>
 						<p>
 							Want to receive the latest news on games!!! Please
 							sign up now!!! You will enjoy the awsome content
 							provided to you!! The best games from the past,
-							present and future are here
-							nooooowwwwwwwwwwwww!!!!!!!!!!
+							present and future are here nooooowwwwwwww!!!!!!!!!!
 						</p>
 						<button
 							type="button"
 							className="btn btn-primary btn-lg">
 							Sign Up
 						</button>
+						<hr />
+						<h1>Login</h1>
 						<p>If already a member please login!!!</p>
 						<button
 							type="button"
@@ -77,53 +79,57 @@ const Home = () => {
 				style={{ color: "wheat" }}>
 				Games Of The Year
 			</h2>
-			<div
-				id="carouselExampleControls"
-				class="carousel slide"
-				data-bs-ride="carousel">
-				<div class="carousel-inner">
-					<div class="carousel-item active">
-						<img
-							src="https://images.immediate.co.uk/production/volatile/sites/3/2021/09/spider-man-2-2023-4255476.png"
-							className="d-block w-100"
-							alt="spider"
-						/>
+			<div className="row">
+				<div className="col-2"></div>
+				<div
+					id="carouselExampleControls"
+					className="carousel slide col-8"
+					data-bs-ride="carousel">
+					<div className="carousel-inner">
+						<div class="carousel-item active">
+							<img
+								src="https://images.immediate.co.uk/production/volatile/sites/3/2021/09/spider-man-2-2023-4255476.png"
+								className="d-block w-100"
+								alt="spider"
+							/>
+						</div>
+						<div className="carousel-item">
+							<img
+								src="https://static0.srcdn.com/wordpress/wp-content/uploads/2020/07/kratos-god-of-war-.jpg"
+								className="d-block w-100"
+								alt="god-of-war"
+							/>
+						</div>
+						<div className="carousel-item">
+							<img
+								src="https://media.gq-magazine.co.uk/photos/5d13ae679fa6014038839ef1/master/pass/red-dead-redemption-03-22oct18_b.jpg"
+								className="d-block w-100"
+								alt="red-dead"
+							/>
+						</div>
 					</div>
-					<div class="carousel-item">
-						<img
-							src="https://static0.srcdn.com/wordpress/wp-content/uploads/2020/07/kratos-god-of-war-.jpg"
-							className="d-block w-100"
-							alt="god-of-war"
-						/>
-					</div>
-					<div class="carousel-item">
-						<img
-							src="https://media.gq-magazine.co.uk/photos/5d13ae679fa6014038839ef1/master/pass/red-dead-redemption-03-22oct18_b.jpg"
-							className="d-block w-100"
-							alt="red-dead"
-						/>
-					</div>
+					<button
+						className="carousel-control-prev"
+						type="button"
+						data-bs-target="#carouselExampleControls"
+						data-bs-slide="prev">
+						<span
+							className="carousel-control-prev-icon"
+							aria-hidden="true"></span>
+						<span className="visually-hidden">Previous</span>
+					</button>
+					<button
+						className="carousel-control-next"
+						type="button"
+						data-bs-target="#carouselExampleControls"
+						data-bs-slide="next">
+						<span
+							className="carousel-control-next-icon"
+							aria-hidden="true"></span>
+						<span className="visually-hidden">Next</span>
+					</button>
 				</div>
-				<button
-					className="carousel-control-prev"
-					type="button"
-					data-bs-target="#carouselExampleControls"
-					data-bs-slide="prev">
-					<span
-						className="carousel-control-prev-icon"
-						aria-hidden="true"></span>
-					<span className="visually-hidden">Previous</span>
-				</button>
-				<button
-					className="carousel-control-next"
-					type="button"
-					data-bs-target="#carouselExampleControls"
-					data-bs-slide="next">
-					<span
-						className="carousel-control-next-icon"
-						aria-hidden="true"></span>
-					<span className="visually-hidden">Next</span>
-				</button>
+				<div className="col-2"></div>
 			</div>
 			<h2
 				className="gamesOfYear"
